f83b3beaa0e61a38636f0f7a290f9a1da535b2d7
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-10-17 09:57:48 -0600

    aa

--- js/datos.js
+++ js/datos.js
@@ -14,7 +14,7 @@ function enviarDatos() {
         data.append('email',email.value.trim())
         data.append('contraseña',contrasena.value.trim())
 
-        fetch('/dao/connection.php', {
+        fetch('../dao/registroUsuario.php', {
             method: 'POST',
             body: data
         })
