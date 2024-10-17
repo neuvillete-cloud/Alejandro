c80aa4470a5b5ae27758112490a4ee99fec1caa5
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-10-17 11:15:50 -0600

    a

--- dao/registroUsuario.php
+++ dao/registroUsuario.php
@@ -9,15 +9,11 @@ if(isset($_POST['nomina'], $_POST['nombre'], $_POST['email'], $_POST['contrasena
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $contrasena = $_POST['contrasena'];
-    //$response = RegistrarUsuario($nomina, $nombre, $email, $contrasena);
-    $response = $nomina . $nombre . $email . $contrasena;
+    $response = RegistrarUsuario($nomina, $nombre, $email, $contrasena);
+
 } else {
-    //$response = array('status' => 'error', 'message' => 'Error faltan datos');
-    $nomina = $_POST['nomina'];
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $contrasena = $_POST['contrasena'];
-    $response = $nomina . $nombre . $email . $contrasena;
+    $response = array('status' => 'error', 'message' => 'Error faltan datos');
+
 }
 
 echo json_encode($response);
