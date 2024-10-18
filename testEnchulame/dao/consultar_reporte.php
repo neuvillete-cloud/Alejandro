561db287a424ae134d77bd5ed7765ae594adf83b
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-10-18 15:04:12 -0600

    intento 1

--- testEnchulame/dao/consultar_reporte.php
+++ testEnchulame/dao/consultar_reporte.php
@@ -7,8 +7,16 @@ try {
     $con = new LocalConector();
     $conex = $con->conectar();
 
+    if ($conex->connect_error) {
+        throw new Exception('Error de conexión: ' . $conex->connect_error);
+    }
+
     $query = "SELECT id, objeto, fecha, descripcion, area FROM Reporte";
     $resultado = $conex->query($query);
+
+    if (!$resultado) {
+        throw new Exception('Error en la consulta: ' . $conex->error);
+    }
 
     $reportes = array();
     if ($resultado->num_rows > 0) {
@@ -18,9 +26,16 @@ try {
     }
 
     $conex->close();
-    echo json_encode($reportes);
+
+    $json = json_encode($reportes);
+    if ($json === false) {
+        throw new Exception('Error al codificar JSON: ' . json_last_error_msg());
+    }
+
+    echo $json;
 } catch (Exception $e) {
     echo json_encode(array("error" => $e->getMessage()));
 }
 exit;
 ?>
+
