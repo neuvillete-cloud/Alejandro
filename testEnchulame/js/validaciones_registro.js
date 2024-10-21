31c0e09c6e78064d7fa61caf386e3555abba5111
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-10-21 14:38:02 -0600

    intento

--- testEnchulame/js/validaciones_registro.js
+++ testEnchulame/js/validaciones_registro.js
@@ -24,7 +24,7 @@ document.getElementById('registerBtn').addEventListener('click', function () {
     }
 
     // Enviar los datos al PHP
-    fetch('dao/registroUsuarios.php', {
+    fetch('dao/registroUsuario.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
