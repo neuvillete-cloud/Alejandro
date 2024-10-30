6dfed64552c5cfce867970da00d9d244acea26d6
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-10-30 13:12:21 -0600

    intento

--- enchulame1/registrate.php
+++ enchulame1/registrate.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <link rel="stylesheet" href="css/estilosRegistro.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
 <div class="container">
@@ -37,6 +38,6 @@
     </div>
 </div>
 <script src="js/validacionesRegistrate.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </body>
 </html>
