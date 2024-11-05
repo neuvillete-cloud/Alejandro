31e5cff7a90f3a7c94455475956ac3855edcb392
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-11-05 12:59:47 -0600

    intento

--- enchulame1/restablecerContrasena.php
+++ enchulame1/restablecerContrasena.php
@@ -24,16 +24,17 @@
         <input type="password" id="confirmaContrasena" name="confirmaContrasena" required placeholder="Confirma tu nueva contraseña">
 
         <button type="button" id="restablecerContrasenaBtn" name="restablecerContrasenaBtn" class="btn login" onclick="actualizarPassword()">Actualizar contraseña</button>
+
+        <!-- Enlace para ir a iniciar sesión -->
+        <p id="loginRedirect" style="display: none; margin-top: 1rem;">
+            <a href="#" onclick="redireccionarLogin()" class="login-link">Ir a Iniciar Sesión</a>
+        </p>
     </form>
 
     <p id="errorMessage" style="display: none;">Las contraseñas no coinciden.</p>
-
-    <!-- Enlace para ir a iniciar sesión -->
-    <p id="loginRedirect" style="display: none;">
-        <a href="#" onclick="redireccionarLogin()" class="login-link">Ir a Iniciar Sesión</a>
-    </p>
 </div>
 
 <script src="js/RestableceContrasena.js"></script>
 </body>
 </html>
+
