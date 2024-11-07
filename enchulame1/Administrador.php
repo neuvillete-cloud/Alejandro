e07c4b8da2e6e4ae21609b8fefaa733cbffd0525
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-11-07 14:46:04 -0600

    intento

--- enchulame1/Administrador.php
+++ enchulame1/Administrador.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador - Reportes</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Vincula tu archivo CSS -->
+    <link rel="stylesheet" href="css/estilosAdministrador.css"> <!-- Vincula tu archivo CSS -->
 </head>
 <body>
 <div class="container">
