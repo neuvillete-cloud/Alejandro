b7b489a96e381eb62fd0e42fcee3670eb9a8eb5b
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-11-13 15:17:11 -0600

    intento

--- enchulame1/detallesReporte.php
+++ enchulame1/detallesReporte.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalles del Reporte</title>
     <link rel="stylesheet" href="css/estilosDetallesReporte.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!-- SweetAlert2 para mensajes -->
 </head>
 <body>
 <header>
