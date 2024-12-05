077e64dbf9833a9d22502d87e65b8938ff176bc8
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-12-05 15:11:04 -0600

    intento

--- enchulame1/tablaSolicitante.php
+++ enchulame1/tablaSolicitante.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial - Reportes</title>
-    <link rel="stylesheet" href="css/estilosAdministrador.css"> <!-- Vincula tu archivo CSS -->
+    <link rel="stylesheet" href="css/estilosHistorial.css"> <!-- Vincula tu archivo CSS -->
 </head>
 <body>
 <div class="container">
