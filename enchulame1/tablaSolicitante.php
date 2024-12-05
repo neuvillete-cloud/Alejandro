9a91bf15ac80cc3cfb772452199e2b3594d5b4a0
neuvillete-cloud <ale.tor.jim@gmail.com>
2024-12-05 08:49:00 -0600

    intento

--- enchulame1/tablaSolicitante.php
+++ enchulame1/tablaSolicitante.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador - Reportes</title>
+    <title>Historial - Reportes</title>
     <link rel="stylesheet" href="css/estilosAdministrador.css"> <!-- Vincula tu archivo CSS -->
 </head>
 <body>
