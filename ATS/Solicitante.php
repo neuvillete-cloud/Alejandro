1c679ae76988fbd747dfcad508d86123aa53069f
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-01-22 15:28:46 -0600

    intento

--- ATS/Solicitante.php
+++ ATS/Solicitante.php
@@ -95,25 +95,6 @@ session_start();
         profileDropdown.classList.toggle('active');
     });
 
-    const viewProfile = document.getElementById('viewProfile');
-    const profileModal = document.getElementById('profileModal');
-    const closeModal = document.getElementById('closeModal');
-
-    viewProfile.addEventListener('click', (e) => {
-        e.preventDefault();
-        profileModal.style.display = 'flex';
-    });
-
-    closeModal.addEventListener('click', () => {
-        profileModal.style.display = 'none';
-    });
-
-    window.addEventListener('click', (e) => {
-        if (e.target === profileModal) {
-            profileModal.style.display = 'none';
-        }
-    });
-
 </script>
 </body>
 </html>
