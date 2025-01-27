8e37bc920be57cca55edfa89ea11956c7b521e91
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-01-27 11:20:50 -0600

    intento

--- ATS/js/jsSolicitante.js
+++ ATS/js/jsSolicitante.js
@@ -39,7 +39,7 @@ document.getElementById('solicitudForm').addEventListener('submit', function (ev
     }
 
     // Enviar los datos al servidor mediante fetch
-    fetch('dao/registrarSolicitud.php', {
+    fetch('dao/reegistroSolicitud.php', {
         method: 'POST',
         body: formData
     })
