1e72e0b862e99625557eecb4266f84804a0e0dea
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-04 10:56:56 -0600

    intento

--- ATS/Solicitante.php
+++ ATS/Solicitante.php
@@ -154,13 +154,6 @@ if (!isset($_SESSION['NumNomina'])) {
 
 
 </script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 <script src="js/pestanas.js"></script>
 <script src="js/funcionamientoModal.js"></script>
 <script src="js/jsSolicitante.js"></script>
