7c0e484700c6a6091316c0effb878cf53b84a816
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-04 10:42:59 -0600

    intento

--- ATS/Solicitante.php
+++ ATS/Solicitante.php
@@ -149,20 +149,16 @@ if (!isset($_SESSION['NumNomina'])) {
 
 
 </script>
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery.dataTables.min.js"></script>
-<script src="js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 <script src="js/pestanas.js"></script>
 <script src="js/funcionamientoModal.js"></script>
 <script src="js/jsSolicitante.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- ATS/historicos.php
+++ ATS/historicos.php
@@ -1,92 +1,137 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/dataTables.bootstrap4.css">
+    <title>Title</title>
+
+    <script src="js/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css" />
+    <script type="text/javascript" src="js/datatables.min.js"></script>
 </head>
 <body>
 
-<table class="table datatables" id="dataTable-1">
+<table id="example" tableexport-key="tabla" class="table display">
     <thead>
     <tr>
-        <th>Folio</th>
-        <th>Número de Parte</th>
-        <th>Primer Conteo</th>
-        <th>Segundo Conteo</th>
+        <th>Nomina</th>
+        <th>Nombre</th>
+        <th>Puesto</th>
+        <th>Puesto</th>
     </tr>
     </thead>
     <tfoot>
     <tr>
-        <th>Folio</th>
-        <th>Número de Parte</th>
-        <th>Primer Conteo</th>
-        <th>Segundo Conteo</th>
+        <th>Filter..</th>
+        <th>Filter..</th>
+        <th>Filter..</th>
+        <th>Filter..</th>
     </tr>
     </tfoot>
-    <tbody>
-
-    </tbody>
 </table>
 
 
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+
 <script>
-    $.ajax({
-        url: 'https://grammermx.com/AleTest/ATS/dao/daoSoli.php', // Reemplaza esto con la URL de tus datos
-        dataType: 'json',
-        success: function(data) {
-            var table = $('#dataTable-1').DataTable({
-                data: data.data,
-                columns: [
-                    { data: 'NumNomina' },
-                    { data: 'IdArea' },
-                    { data: 'Puesto' },
-                    { data: 'TipoContratacion' }
-                ],
-                autoWidth: true,
-                "lengthMenu": [
-                    [16, 32, 64, -1],
-                    [16, 32, 64, "All"]
-                ],
-                dom: 'Bfrtip',
-                buttons: [
-                    {
-                        extend: 'copy',
-                        className: 'btn btn-sm copyButton'
-                    },
-                    {
-                        extend: 'csv',
-                        className: 'btn btn-sm csvButton'
-                    },
-                    {
-                        extend: 'excel',
-                        className: 'btn btn-sm excelButton'
-                    },
-                    {
-                        extend: 'pdf',
-                        className: 'btn btn-sm pdfButton'
-                    },
-                    {
-                        extend: 'print',
-                        className: 'btn btn-sm printButton'
-                    }
-                ],
-                initComplete: function () {
-                    this.api().columns().every( function () {
-                        var column = this;
-                        var input = document.createElement("input");
-                        input.className = 'form-control form-control-sm';
-                        $(input).appendTo($(column.footer()).empty())
-                            .on('keyup change clear', function () {
-                                if (column.search() !== this.value) {
-                                    column.search(this.value).draw();
-                                }
-                            });
+    $(document).ready(function () {
+
+        $('#example tfoot th').each(function () {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="Filtrar..." class="form-control input-sm" size="3px" />');
+        });
+        var tabla = $('#example').DataTable({
+            "responsive": true,
+            "ajax": {
+                "url": 'https://grammermx.com/AleTest/ATS/dao/daoSoli.php',
+                "dataSrc": "data"
+            },
+            "columns": [
+                { "data": "NumNomina" },
+                { "data": "IdArea" },
+                { "data": "Puesto" },
+                { "data": "TipoContratacion" }
+            ],
+            "initComplete": function () {
+                this.api().columns().every(function () {
+                    var that = this;
+                    $('input', this.footer()).on('keyup change', function () {
+                        if (that.search() !== this.value) {
+                            that
+                                .search(this.value)
+                                .draw();
+                        }
                     });
+                });
+            },
+            dom: 'lBfrtip',
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    text: 'Copiar',
+                    exportOptions: {
+                        columns: [ 0, ':visible' ]
+                    },
+                    titleAttr: 'Copiar Texto',
+                    className: 'btn btn-secondary'
+                },
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: [0, ':visible']
+                    },
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: [0, ':visible']
+                    },
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL'
                 }
-            });
-        }
+                /*
+                {
+                    text: 'Seleccione las columnas',
+                    extend: 'colvis',
+                    className: 'btn btn-info'
+                }*/
+            ],
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+            "loadingRecords": "Cargando...",
+            "deferRender": true,
+            "search": {
+                "regex": true,
+                "caseInsensitive": true,
+            },
+        });
+
+
+        $('#min').datepicker({ onSelect: function () { table.draw(); }, changeMonth: true, changeYear: true });
+        $('#max').datepicker({ onSelect: function () { table.draw(); }, changeMonth: true, changeYear: true });
+        //  var table = $('#example').DataTable();
+
+        $('#min, #max').change(function () {
+            table.draw();
+        });
+        //
     });
 </script>
 </body>
