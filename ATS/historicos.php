d1c4401b0519e7f1069fae3f02ae70317f8dd3cd
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-04 14:49:17 -0600

    intento

--- ATS/historicos.php
+++ ATS/historicos.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['NumNomina'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historicos</title>
-    <link rel="stylesheet" href="css/estilosHistoricos.css">
+
     <script src="js/jquery.min.js"></script>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
