7c3e51808013ce4e6f4610444cbddc5aa8776dd6
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-04 09:35:09 -0600

    intento

--- ATS/historicos.php
+++ ATS/historicos.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <link rel="stylesheet" href="css/estilosSolicitante.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
 </head>
 <body>
 
