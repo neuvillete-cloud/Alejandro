3c30d5c47342a2c83d9b26a3a2bf3dde8326f230
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-04 09:15:26 -0600

    intento

---
+++ ATS/dao/daoSoli.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once('db/db_Inventario.php');
+
+
+//$marbete = $_GET['marbete'];
+
+ContadorApu();
+
+
+function ContadorApu()
+{
+    $con = new LocalConector();
+    $conex = $con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT * FROM `Solicitudes`");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    echo json_encode(array("data" => $resultado));
+}
+
+
+?>
--- ATS/historicos.php
+++ ATS/historicos.php
@@ -1,1 +1,101 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/dataTables.bootstrap4.css">
+</head>
+<body>
+
+<table class="table datatables" id="dataTable-1">
+    <thead>
+    <tr>
+        <th>Folio</th>
+        <th>Número de Parte</th>
+        <th>Primer Conteo</th>
+        <th>Segundo Conteo</th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+        <th>Folio</th>
+        <th>Número de Parte</th>
+        <th>Primer Conteo</th>
+        <th>Segundo Conteo</th>
+    </tr>
+    </tfoot>
+    <tbody>
+
+    </tbody>
+</table>
+
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
+<script>
+    $.ajax({
+        url: 'https://grammermx.com/AleTest/ATS/dao/daoSoli.php', // Reemplaza esto con la URL de tus datos
+        dataType: 'json',
+        success: function(data) {
+            var table = $('#dataTable-1').DataTable({
+                data: data.data,
+                columns: [
+                    { data: 'NumNomina' },
+                    { data: 'IdArea' },
+                    { data: 'Puesto' },
+                    { data: 'TipoContratacion' }
+                ],
+                autoWidth: true,
+                "lengthMenu": [
+                    [16, 32, 64, -1],
+                    [16, 32, 64, "All"]
+                ],
+                dom: 'Bfrtip',
+                buttons: [
+                    {
+                        extend: 'copy',
+                        className: 'btn btn-sm copyButton'
+                    },
+                    {
+                        extend: 'csv',
+                        className: 'btn btn-sm csvButton'
+                    },
+                    {
+                        extend: 'excel',
+                        className: 'btn btn-sm excelButton'
+                    },
+                    {
+                        extend: 'pdf',
+                        className: 'btn btn-sm pdfButton'
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn btn-sm printButton'
+                    }
+                ],
+                initComplete: function () {
+                    this.api().columns().every( function () {
+                        var column = this;
+                        var input = document.createElement("input");
+                        input.className = 'form-control form-control-sm';
+                        $(input).appendTo($(column.footer()).empty())
+                            .on('keyup change clear', function () {
+                                if (column.search() !== this.value) {
+                                    column.search(this.value).draw();
+                                }
+                            });
+                    });
+                }
+            });
+        }
+    });
+</script>
+</body>
+</html>
