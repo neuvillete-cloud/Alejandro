389cb6dba3e2fd264b7d440e1b4861cb2d919c7b
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-07 09:57:47 -0600

    intento

--- ATS/seguimiento.php
+++ ATS/seguimiento.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Seguimiento - Porcentajes</title>
+    <link rel="stylesheet" href="css/estilosSolicitante.css">
 </head>
 <body>
 
