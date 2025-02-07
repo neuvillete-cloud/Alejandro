f22637dc9b1732c81e444e45a0355049f5a1418d
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-07 12:41:45 -0600

    intento

--- ATS/seguimiento.php
+++ ATS/seguimiento.php
@@ -163,7 +163,6 @@ if (!isset($_SESSION['NumNomina'])) {
         });
     });
 </script>
-<script src="js/funcionamientoModal.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
