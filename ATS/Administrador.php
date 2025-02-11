605109d34b4f66d641d05e38b7c2309ebe186f9d
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-11 09:22:38 -0600

    intento

--- ATS/Administrador.php
+++ ATS/Administrador.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['NumNomina'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Históricos</title>
-    <link rel="stylesheet" href="css/estilosHistoricos.css">
+    <link rel="stylesheet" href="css/estilosAdministrador.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
