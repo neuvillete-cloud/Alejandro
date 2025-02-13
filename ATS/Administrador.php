33469b2bc91b4efb12a24c88da8fda0cdbd4e3e8
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-13 11:34:05 -0600

    intento

--- ATS/Administrador.php
+++ ATS/Administrador.php
@@ -197,6 +197,7 @@ if (!isset($_SESSION['NumNomina'])) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.1/xlsx.full.min.js"></script>
 
 <script src="js/funcionamientoModal.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function () {
         var tabla = $('#solicitudesTable').DataTable({
