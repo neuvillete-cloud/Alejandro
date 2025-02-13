09f9139161fa6896415c0d19d056051b43d8c24e
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-13 13:14:31 -0600

    intento

--- ATS/Administrador.php
+++ ATS/Administrador.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['NumNomina'])) {
     <!-- Bootstrap CSS -->
 
     <link rel="stylesheet" href="css/estilosAdministrador.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
