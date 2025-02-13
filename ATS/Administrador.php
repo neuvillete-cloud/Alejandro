ad705f8cf50a82cd1439c31c3d2bc233a64c3f95
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-13 12:10:43 -0600

    intento

--- ATS/Administrador.php
+++ ATS/Administrador.php
@@ -184,6 +184,8 @@ if (!isset($_SESSION['NumNomina'])) {
 </script>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap JS (Asegúrate de incluirlo) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery.dataTables.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/SheetJS/0.17.1/xlsx.full.min.js"></script>
