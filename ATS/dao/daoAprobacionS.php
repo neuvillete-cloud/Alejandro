b0ffc23ab066342256585d5e7c9566aa50a6b35b
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-02-27 11:39:28 -0600

    intento

--- ATS/dao/daoAprobacionS.php
+++ ATS/dao/daoAprobacionS.php
@@ -1,5 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include_once("ConexionBD.php");
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['nombreAprobador'], $_POST['accion'], $_POST['folio'])) {
