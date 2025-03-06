be28d04b0f07e689b65eb6072a889c420e3f2f52
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-03-06 13:23:14 -0600

    intento

--- ATS/SAprobadas.php
+++ ATS/SAprobadas.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION['NumNomina'])) {
 <!-- Sidebar -->
 <nav class="sidebar" id="sidebar">
     <ul>
-        <li><a href="Solicitante.php">Inicio</a></li>
+        <li><a href="Administrador.php">Inicio</a></li>
         <li><a href="SeguimientoAdministrador.php">Seguimiento</a></li>
         <li><a href="SAprobadas.php">Históricos</a></li>
         <li><a href="configuraciones.php">Configuraciones</a></li>
