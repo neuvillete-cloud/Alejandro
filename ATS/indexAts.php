afc217e1fc57c5b1f590fb5ca22a8d8178c88519
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-03-25 12:01:20 -0600

    intento

---
+++ ATS/indexAts.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siemens Energy Clone</title>
+    <link rel="stylesheet" href="css/indexAts.css">
+</head>
+<body>
+<header>
+    <div class="logo">
+        <h1>SIEMENS</h1>
+        <span>energy</span>
+    </div>
+    <nav>
+        <a href="#">Buscar empleos</a>
+        <a href="#">Acerca de nosotros</a>
+        <a href="#">Programa de posgrado</a>
+        <a href="#">Inclusión y diversidad</a>
+        <a href="#">Inicio de sesión</a>
+        <a href="#">🌐 Español</a>
+    </nav>
+</header>
+
+<div class="center-content">
+    <h2>Hagamos que el mañana sea diferente, hoy mismo.</h2>
+    <div class="search-box">
+        <input type="text" placeholder="">
+        <button>Search Jobs</button>
+    </div>
+</div>
+</body>
+</html>
