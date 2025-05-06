241d9a0696d06ff587c81c9428fe8bcf6db909a1
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-05-06 12:06:09 -0600

    intento

---
+++ ATS/Registro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Siemens Energy Clone Mejorado</title>
+    <link rel="stylesheet" href="css/Registro.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+<body>
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <h1>Grammer</h1>
+            <span>Automotive</span>
+        </div>
+        <nav>
+            <a href="#">Buscar empleos</a>
+            <a href="aboutUs.php">Acerca de nosotros</a>
+            <a href="practicantes.php">Programa de posgrado</a>
+            <a href="#">Inclusión y diversidad</a>
+            <a href="#">Inicio de sesión</a>
+            <a href="#">🌐 Español ▾</a>
+        </nav>
+    </div>
+</header>
+
+<section class="section-title">
+    <h1>Inscripcion</h1>
+</section>
