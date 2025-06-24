403c290c81e6056ebfe3afc7cb6afedbdf101fbf
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-06-24 12:31:35 -0600

    intento

--- ATS/postularme.php
+++ ATS/postularme.php
@@ -130,7 +130,7 @@ session_start();
 
     </div>
 </section>
-<script src="js/vacanteDinamica.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.17/js/intlTelInput.min.js"></script>
 
