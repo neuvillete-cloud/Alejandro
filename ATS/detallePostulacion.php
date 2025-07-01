991dfd54c1200fdb6b532bf463846f82ccb26f0e
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-01 13:14:50 -0600

    intento

--- ATS/detallePostulacion.php
+++ ATS/detallePostulacion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Postulación - Vista previa CV</title>
+    <title>Detalles</title>
     <link rel="stylesheet" href="css/postularme.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
