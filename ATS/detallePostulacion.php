f0804854d7bdbc9b031dce75ad019f08d6fffa2e
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-02 10:40:40 -0600

    intento

--- ATS/detallePostulacion.php
+++ ATS/detallePostulacion.php
@@ -118,7 +118,7 @@ session_start();
         console.log("ID:", IdPostulacion); // <-- pon esto antes del fetch
 
 
-        fetch(`/AleTest/ATS/dao/ObtenerPostulacion.php?IdPostulacion=${IdPostulacion}`)
+        fetch(`/dao/ObtenerPostulacion.php?IdPostulacion=${IdPostulacion}`)
 
                 .then(response => response.json())
             .then(data => {
