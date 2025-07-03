1a1ddf9fa735aee2881d690a943f48ccc3f85cde
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-03 12:29:22 -0600

    intento

--- ATS/seleccionFinal.php
+++ ATS/seleccionFinal.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['NumNomina'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Candidatos Finales</title>
-    <link rel="stylesheet" href="css/postulaciones.css">
+    <link rel="stylesheet" href="css/seleccionFinal.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
