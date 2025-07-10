ccff093b74c9ce7b0f4356f5cb74634cebb1bb13
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-10 10:02:28 -0600

    intento

--- ATS/Registro.php
+++ ATS/Registro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Siemens Energy Clone Mejorado</title>
+    <title>Grammer Automotive Registro</title>
     <link rel="stylesheet" href="css/Registro.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
