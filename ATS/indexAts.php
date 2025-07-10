ee1c31a0aba465070f8b2e30d09132c287b261f8
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-10 09:55:37 -0600

    intento

--- ATS/indexAts.php
+++ ATS/indexAts.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Siemens Energy Clone Mejorado</title>
+    <title>Grammer Automotive</title>
     <link rel="stylesheet" href="css/indexAts.css">
 </head>
 <body>
