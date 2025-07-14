047221c533f563091673cfc6ddb1b728f445f06d
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-14 10:53:04 -0600

    intento

--- ATS/HistorialUsuario.php
+++ ATS/HistorialUsuario.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vacantes en Grammer Automotive</title>
-    <link rel="stylesheet" href="css/HistorialUsuario.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/HistorialUsuario.css">
 </head>
 <body>
 
