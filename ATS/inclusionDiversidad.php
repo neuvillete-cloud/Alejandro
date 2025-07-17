5509d9675cecb47a9ad5114ebad669f07e99c9f6
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-17 11:33:43 -0600

    intento

--- ATS/inclusionDiversidad.php
+++ ATS/inclusionDiversidad.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Inclusion y Diversidad</title>
-    <link rel="stylesheet" href="css/practicantes.css" />
+    <link rel="stylesheet" href="css/Inclusion.css" />
 </head>
 <body>
 <div class="container">
