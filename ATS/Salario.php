87c66a9b77623b208a48e86b5f9654cc347f82a3
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-07-21 10:46:11 -0600

    intento

---
+++ ATS/Salario.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grammer Automotive login</title>
+    <link rel="stylesheet" href="css/loginAts.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+</head>
+<body>
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <img src="imagenes/logo_blanco.png" alt="Logo Grammer" class="logo-img">
+            <div class="logo-texto">
+                <h1>Grammer</h1>
+                <span>Automotive</span>
+            </div>
+        </div>
+        <nav>
+            <a href="indexAts.php">Buscar empleos</a>
+            <a href="aboutUs.php">Acerca de nosotros</a>
+            <a href="practicantes.php">Escuela de Talentos</a>
+            <a href="#">Inclusión y diversidad</a>
+            <a href="loginATS.php">Inicio de sesión</a>
+        </nav>
+    </div>
+</header>
+
+<!-- AQUÍ INICIA EL LOGIN -->
+
+<!-- Header sigue fijo arriba -->
+
+<section class="section-title">
+    <h1>Calculadora de ISR</h1>
+    <img src="imagenes/iniciar-sesion.png" alt="Imagen decorativa" class="imagen-banner">
+</section>
+
