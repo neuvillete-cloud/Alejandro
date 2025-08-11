6a32f305af7807947fc3a388aa467720908634ca
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-08-11 13:22:19 -0600

    intento

--- ATS/candidatoSeleccionado.php
+++ ATS/candidatoSeleccionado.php
@@ -117,7 +117,6 @@ if (!isset($_SESSION['NumNomina'])) {
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="js/candidatoSeleccionado.js"></script>
-<script src="js/funcionamientoModal.js"></script>
 <script>
     const logoutLink = document.getElementById('logout');
     if (logoutLink) {
