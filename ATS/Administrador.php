2f38c3365dd05941d9ea4b6c1c32735475f31d2b
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-08-15 14:11:58 -0600

    intento

--- ATS/Administrador.php
+++ ATS/Administrador.php
@@ -166,7 +166,6 @@ if (!isset($_SESSION['NumNomina'])) {
 <!-- SheetJS (para exportar a Excel) -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.1/xlsx.full.min.js"></script>
 
-<script src="js/funcionamientoModal.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     $(document).ready(function () {
