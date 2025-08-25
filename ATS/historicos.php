ca75c18e76ad786b1953ca5f5d2ef4b419b45df2
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-08-25 15:33:04 -0600

    intento

--- ATS/historicos.php
+++ ATS/historicos.php
@@ -26,9 +26,9 @@ if (!isset($_SESSION['NumNomina'])) {
             </div>
         </div>
         <nav>
-            <a href="#">Nueva Solicitud</a>
+            <a href="Solicitante.php">Nueva Solicitud</a>
             <a href="seguimiento.php">Seguimiento</a>
-            <a href="historicos.php">Historial de Solicitudes</a>
+            <a href="#">Historial de Solicitudes</a>
             <a href="seleccionFinal.php">Candidatos Finales</a>
 
             <?php if (isset($_SESSION['Nombre'])): ?>
