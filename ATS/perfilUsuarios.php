a341bcdd4db721041e957cfe7063e2db813564f6
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-08-26 11:06:26 -0600

    intento

--- ATS/perfilUsuarios.php
+++ ATS/perfilUsuarios.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['NumNomina'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Perfil | ATS Grammer</title>
-    <link rel="stylesheet" href="css/estilosPerfilUsuarios.css">
+    <link rel="stylesheet" href="css/estilosPerfilATS.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
