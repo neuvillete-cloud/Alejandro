f57b0ea8e6de67a54c6c32bc51398071999eadf7
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-09-01 14:52:43 -0600

    intento

--- ATS/inclusionDiversidad.php
+++ ATS/inclusionDiversidad.php
@@ -247,6 +247,48 @@
         });
     });
 </script>
+<footer class="main-footer">
+    <div class="footer-container">
+
+        <div class="footer-column">
+            <div class="logo">
+                <img src="imagenes/logo_blanco.png" alt="Logo Grammer Blanco" class="logo-img">
+                <div class="logo-texto">
+                    <h1>Grammer</h1>
+                    <span>Automotive</span>
+                </div>
+            </div>
+            <p class="footer-about">
+                Sistema de Seguimiento de Candidatos (ATS) para la gestión de talento y requisiciones de personal.
+            </p>
+        </div>
+
+        <div class="footer-column">
+            <h3>Enlaces Rápidos</h3>
+            <ul class="footer-links">
+                <li><a href="indexAts.php">Inicio</a></li>
+                <li><a href="aboutUs.php">Acerca de Nosotros</a></li>
+                <li><a href="practicantes.php">Escuela de Talentos</a></li>
+                <li><a href="inclusionDiversidad.php">Inclusion y Diversidad</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-column">
+            <h3>Contacto</h3>
+            <p><i class="fas fa-map-marker-alt"></i> Av. de la Luz #24 Col. satélite , Querétaro, Mexico</p>
+            <p><i class="fas fa-phone"></i> +52 (442) 238 4460</p>
+            <div class="social-icons">
+                <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiA6MqY0KaPAxUmlGoFHX01AXwQFnoECD0QAQ&url=https%3A%2F%2Fwww.facebook.com%2Fgrammermexico%2F%3Flocale%3Des_LA&usg=AOvVaw1Jg2xRElzuIF1PIZ6Ip_Ms&opi=89978449" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://mx.linkedin.com/company/grammer-automotive-puebla-s-a-de-c-v-" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://www.instagram.com/grammerqro/" aria-label="Instagram"><i class="fab fa-instagram"></i></a>            </div>
+        </div>
+
+    </div>
+    <div class="sub-footer">
+        <p>&copy; <?= date('Y') ?> Grammer Automotive de México. Todos los derechos reservados.</p>
+        <p class="developer-credit">Desarrollado con <i class="fas fa-heart"></i> por Alejandro Torres Jimenez</p>
+    </div>
+</footer>
 </body>
 </html>
 
