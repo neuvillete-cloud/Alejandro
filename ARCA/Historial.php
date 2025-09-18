0d5b61b07a1ed7643004629cf52c4d2fd30f5e72
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-09-18 13:21:44 -0600

    intento

--- ARCA/Historial.php
+++ ARCA/Historial.php
@@ -26,7 +26,7 @@ $solicitudes = $stmt->get_result();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Solicitudes - ARCA</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilosHistorial.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
