0112dc33e2ed013070b2bd39aa8021eb43e32444
neuvillete-cloud <ale.tor.jim@gmail.com>
2025-10-01 15:38:30 -0600

    intento

--- ARCA/Historial.php
+++ ARCA/Historial.php
@@ -111,7 +111,7 @@ $conex->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($tituloPagina); ?> - ARCA</title>
-    <link rel="stylesheet" href="css/estilosHistorial.css">
+    <link rel="stylesheet" href="css/estilosHistorial.css?v=1.2">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
